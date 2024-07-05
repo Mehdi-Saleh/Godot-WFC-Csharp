e1bef64047e7331819d6eb6e23b26f2643a66cb6
Mehdi Saleh <94439809+Mehdi-Saleh@users.noreply.github.com>
2024-07-05 19:56:32 +0330

    Imported some new 3D models

--- .godot/mono/temp/obj/Debug/WFC-2d.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/WFC-2d.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WFC-2d")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae4fc250c47665c41f0cbb179bb6063c0cde662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8536f21849cfa53531f0bd6b4296755343cd53")]
 [assembly: System.Reflection.AssemblyProductAttribute("WFC-2d")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WFC-2d")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
